05583d0164c92c476a2febe95bbecd2941c374d5
Matsunaga Takaaki <matsunagatakaaki@se-project.co.jp>
2015-04-16 17:08:38 +0900

    clean

--- matsunagatakaaki/AddressBook/src/helper/Urls.java
+++
@@ -1,7 +0,0 @@
-package helper;
-
-public class Urls {
-	public static final String TOP = "./";
-	public static final String ADDRESS_LIST = "./AddressList";
-	public static final String RESULT = "result.jsp";
-}

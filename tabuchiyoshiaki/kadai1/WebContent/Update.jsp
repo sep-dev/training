b3e90e4ca861171b220222c9747c13daf26d56d9
root <tabuchiyoshiaki@se-project.co.jp>
2015-04-18 18:37:41 +0900

    住所録
    本日の変更点：javascriptのエラー修正

--- tabuchiyoshiaki/kadai1/WebContent/Update.jsp
+++ tabuchiyoshiaki/kadai1/WebContent/Update.jsp
@@ -41,74 +41,24 @@ res.next();
                <%= "住所:" + res.getString("address")%>
                 <%-- レコードのPRICEフィールドを表示 --%>
                 <%= "電話番号:" + res.getString("tel")%>
-<script type="text/javascript">
-<!--
-
-function check(){
-
-	var flag = 0;
-
-
-	// 設定開始（必須にする項目を設定してください）
-
-	if(document.form1.name.value == ""){ // 「お名前」の入力をチェック
-
-		flag += 1;
-
-	}
-	 if(document.form1.address.value == ""){ // 「住所」の入力をチェック
-
-		flag += 1;
-
-	}
-	 if(document.form1.tel.value == ""){ // 「電話番号」の入力をチェック
-
-		flag += 1;
-
-	}
-
-	// 設定終了
-
-
-	if(flag==3){
 
 
 
-			// 「OK」時の処理開始 ＋ 確認ダイアログの表示
-			if(window.confirm('空欄です！入力してください！！\n一覧表示画面に戻ります')){
-
-				location.href = "Tablelist1.jsp";
-
-			}
-			// 「OK」時の処理終了
-
-			// 「キャンセル」時の処理開始
-			else{
-
-				window.alert('キャンセルされました'); // 警告ダイアログを表示
-
-			}
-			// 「キャンセル」時の処理終了
-	}
-}
-
-// -->
-
-</script>
-
-<form method="post" action="" name="form1"  onSubmit="return check()">
-<p>氏名&nbsp;<input type="text" size="40" value=<%=  res.getString("name")%> name="name" id="sample1" /></p>
-   <p>住所&nbsp;<input type="text" size="40" value=<%=  res.getString("address")%> name="address" id="sample1" /></p>
- <p>電話番号&nbsp;<input type="text" size="40" value=<%=  res.getString("tel")%>  name="tel" id="sample1" /></p>
+ <form >
+<p>氏名&nbsp;<input type="text" size="40" name="name"  value=<%=  res.getString("name")%> id="sample1" /></p>
+   <p>住所&nbsp;<input type="text" size="40"  name="address"  value=<%=  res.getString("address")%> id="sample1" /></p>
+ <p>電話番号&nbsp;<input type="text" size="40"   name="tel"  value=<%=  res.getString("tel")%> id="sample1" /></p>
  <br>
-  <input type="reset" name="reset" value="リセット">
- </form>
  <p><button onclick="check()">更新</button>
+<input type="reset" name="reset" value="リセット">
+
+ </form>
 
  <form action="Tablelist1.jsp">
  <input type="submit" name="tablelist" value="一覧表示">
- <input type="submit" name="delete" value="削除">
  </form>
+ <input type="submit" name="delete" value="削除">
+
  <br>
  <%
 }
---
+++ tabuchiyoshiaki/kadai1/src/servlet/Update.java
@@ -0,0 +1,82 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Update extends HttpServlet {
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException{
+	  response.setContentType("text/html; charset=UTF-8");
+	  request.setCharacterEncoding("UTF-8");
+	  String disp = "/Success";
+	  RequestDispatcher dispatch = request.getRequestDispatcher(disp);
+
+	  dispatch.forward(request, response);
+
+    
+    PrintWriter out = response.getWriter();
+
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Registration</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("Registration");
+    Connection conn = null;
+    int id = 9;
+    String url = "jdbc:mysql://localhost/kadai1";
+    String user = "root";
+    String password = "onrain14";
+
+    try {
+
+    	String name = request.getParameter("name") ;
+    	String address = request.getParameter("address") ;
+    	String tel =request.getParameter("tel");
+      Class.forName("com.mysql.jdbc.Driver");
+      conn = DriverManager.getConnection(url, user, password);
+
+      Statement stmt = conn.createStatement();
+
+      String sql = "insert into tbaddress (name,address,tel) values(\"" + name + "\", \"" + address + "\",\"" + tel +"\");";
+      int num = stmt.executeUpdate(sql);
+
+
+
+
+      stmt.close();
+    }catch (ClassNotFoundException e){
+      out.println("ClassNotFoundException:" + e.getMessage());
+    }catch (SQLException e){
+      out.println("SQLException:" + e.getMessage());
+    }catch (Exception e){
+      out.println("Exception:" + e.getMessage());
+    }finally{
+      try{
+        if (conn != null){
+          conn.close();
+        }
+      }catch (SQLException e){
+        out.println("SQLException:" + e.getMessage());
+      }
+    }
+
+    out.println("</body>");
+    out.println("</html>");
+  }
+
+private String readLine() {
+	// TODO 自動生成されたメソッド・スタブ
+	return null;
+}
+}

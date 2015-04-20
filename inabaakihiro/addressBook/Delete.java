f129050ee654ec098b71ad8d2edd75a0392518e3
sep-inabaakihiro <inabaakihiro@se-project.co.jp>
2015-04-20 13:31:23 +0900

    「削除確認画面」で削除ボタンを押し、「削除完了画面」を出力するまでの処理を追加

--- inabaakihiro/addressBook/Delete.java
+++ inabaakihiro/addressBook/Delete.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.DatabaseLogic;
 
 @WebServlet("/Delete")
 public class Delete extends HttpServlet {
@@ -23,7 +26,19 @@ public class Delete extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		DatabaseLogic dbLogic = new DatabaseLogic();
+		dbLogic.connect();
 
+		// 削除対象の会員のIDを取得
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+
+		// 選択されていた会員の、更新対象のデータを更新
+		dbLogic.executeSQL("DELETE FROM ADDRESS_TBL WHERE ID = " + id);
+
+		dbLogic.disconnect();
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/deleteComplete.jsp");
+		dispatcher.forward(request, response);
 	}
-
 }

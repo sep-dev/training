af82d6b38a68a596acbd5f1aad2b41e4e7e07042
sep-inabaakihiro <inabaakihiro@se-project.co.jp>
2015-04-21 13:52:08 +0900

    メニュー切り替えサーブレットの、コメントの変更など

--- inabaakihiro/addressBook/SwitchMenu.java
+++ inabaakihiro/addressBook/SwitchMenu.java
@@ -12,24 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 public class SwitchMenu extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
+	// 各ページで「一覧表示ボタン」または「新規登録ボタン」が押された時
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		// フォワード先
-		String redirectPath = null;
+		String redirectPath = null;		// リダイレクト先
 
-		String action = request.getParameter("action");
+		String action = request.getParameter("action");		// 送信された情報を取得
 
+
+		// 「一覧表示」ボタンが押されたとき
 		if(action == null) {
+
 			// リダイレクト先に, 「新規登録」のJSPファイルを設定
 			redirectPath = "/addressBook/";
 		}
 
+
+		// 「新規登録」ボタンが押されたとき
 		else if(action.equals("list")) {
+
 			// リダイレクト先に, 「一覧表示」のJSPファイルを設定
 			redirectPath = "/addressBook/list.jsp";
 		}
 
+
 		// 設定されたリダイレクト先にリダイレクト
 		response.sendRedirect(redirectPath);
 	}
+
 }
